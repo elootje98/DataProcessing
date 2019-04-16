93abc12e805714bb109c09da90b79f557836cb20
elootje98 <elodierijnja@hotmail.nl>
2019-04-16 15:28:22 +0200

    week_2

--- Homework/Week_2/eda.py
+++ Homework/Week_2/eda.py
@@ -5,80 +5,114 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import json
 
-# Country,Region,Population,Area (sq. mi.),Pop. Density (per sq. mi.),Coastline (coast/area ratio),Net migration,Infant mortality (per 1000 births),"GDP ($ per capita) dollars",Literacy (%),Phones (per 1000),Arable (%),Crops (%),Other (%),Climate,Birthrate,Deathrate,Agriculture,Industry,Service
+def main():
 
-# Load the data into a pandas dataframe
-data_countries = pd.read_csv("input.csv")
+    # Load the data
+    data_countries = load_csv("input.csv")
 
-# Deleting the collums that are not needed.
-data_countries = data_countries.drop(columns=["Population", "Area (sq. mi.)", "Coastline (coast/area ratio)", "Net migration", "Literacy (%)",
-                                               "Literacy (%)", "Phones (per 1000)", "Arable (%)", "Crops (%)", "Other (%)", "Climate", "Birthrate", "Deathrate", "Agriculture", "Industry", "Service"])
+    # Deleting the collums that are not needed.
+    data_countries = data_countries.drop(columns=["Population", "Area (sq. mi.)", "Coastline (coast/area ratio)", "Net migration", "Literacy (%)",
+                                                   "Literacy (%)", "Phones (per 1000)", "Arable (%)", "Crops (%)", "Other (%)", "Climate", "Birthrate", "Deathrate", "Agriculture", "Industry", "Service"])
 
-# Delete the rows that dont contain a value for a certain column.
-data_countries = data_countries.dropna()
-data_countries = data_countries[data_countries['GDP ($ per capita) dollars'] != "unknown"]
+    # Delete the rows that dont contain a value for a certain column.
+    data_countries = data_countries.dropna()
+    data_countries = data_countries[data_countries['GDP ($ per capita) dollars'] != "unknown"]
+    data_countries = data_countries[data_countries['Infant mortality (per 1000 births)'] != "unknown"]
+    data_countries = data_countries[data_countries['Pop. Density (per sq. mi.)'] != "unknown"]
+    data_countries["Region"] = data_countries["Region"].str.replace(" ", "")
+    data_countries["Pop. Density (per sq. mi.)"] = data_countries["Pop. Density (per sq. mi.)"].str.replace(",", ".").astype("float64")
+    data_countries["Infant mortality (per 1000 births)"] = data_countries["Infant mortality (per 1000 births)"].str.replace(",", ".").astype("float64")
+    data_countries['GDP ($ per capita) dollars'] = data_countries['GDP ($ per capita) dollars'].str.lstrip('d').str.rstrip('dollars').astype("int64")
 
-# Remove dollars
-gdp = data_countries['GDP ($ per capita) dollars'].str.lstrip('d').str.rstrip('dollars')
+    # Remove extreme high value in the gdp, therefor its likely to be an incorrect value.
+    # Checked the gdp for this country (Suriname) and its 8043.46 (in 2017).
+    data_countries = data_countries[data_countries['GDP ($ per capita) dollars'] != 400000]
 
-# Calculate the mean
-gdp_int = gdp.astype("int64")
-gdp_int_mean = gdp_int.mean()
-gdp_int_median = gdp_int.median()
-gdp_int_mode = gdp_int.mode()
-deviation = gdp_int.std()
+    # Reset the index of the table
+    data_countries = data_countries.reset_index()
 
-# Make a histogram of the GDP
-countries = data_countries["Country"]
-x = gdp_int
-plt.title("GDP for countries")
-plt.xlabel("GDP")
-plt.ylabel("Countries")
-plt.hist(x, bins=50)
-# plt.show()
+    # Calculate the mean, median and mode
+    gdp_int = data_countries["GDP ($ per capita) dollars"]
+    calculate_tendency(gdp_int)
 
-# Calculate minimum, first quartile, median, third quartile and maximum from infant mortality
-# I THINK THE MAX AND MIN VALUE ARE NOT CORRECT
-# CANT CALCULATE STRING TO FLOAT PROBLEM.
-infant_mortality = data_countries["Infant mortality (per 1000 births)"]
-# infant_mortality = infant_mortality.astype("float64")
-minimum = infant_mortality.min()
-maximum = infant_mortality.max()
-# median = infant_mortality.median()
-# quantile = infant_mortality.quantile()
-print("MIN", minimum)
-print("MAX", maximum)
-# print("MEDIAN", median)
-# print("QUAN", quantile)
+    # Make a histogram of the GDP
+    countries = data_countries["Country"]
+    x = gdp_int
+    plt.title("GDP for countries")
+    plt.xlabel("GDP")
+    plt.ylabel("Countries")
+    plt.hist(x, bins=50)
+    plt.show()
 
-# Write to the json file
-regions = data_countries["Region"]
+    # Calculate minimum, first quartile, median, third quartile and maximum from infant mortality
+    infant_mortality = data_countries["Infant mortality (per 1000 births)"]
+    calculate_five_number(infant_mortality)
 
-print(gdp[1])
-# Make dict of the data from the countries
-# WHY INDEX OUT OF RANGE FOR GDP????????????????
-dict_data = {}
-for i in range(226):
-    country = countries[i]
-    region = regions[i]
-    infant = infant_mortality[i]
-    gdp_int = gdp_int[i]
+    # Make boxplot
+    plt.title("Infant mortality")
+    plt.ylabel("Infant mortality (per 1000 births)")
+    plt.xlabel("")
+    plt.boxplot(infant_mortality)
+    plt.show()
 
-    # Make a python dictionary
-    dict_data[country] = {
-        "Region": region,
-        "Infant": infant,
-        "GDP": gdp
-        }
+    # Write to the json file
+    regions = data_countries["Region"]
+    pop_dens = data_countries['Pop. Density (per sq. mi.)']
 
-# Write the dict_data ot a json file
-with open("data_file.json", "w") as write_file:
-    json.dump(dict_data, write_file)
+    # Make dict of the data from the countries
+    dict_data = {}
+    for i in range(214):
+        country = countries[i]
+        region = regions[i]
+        infant = infant_mortality[i]
+        # You have to transfer it to a real int, since first it was a np int and you cant
+        # write those into the json file.
+        gdp_dict = int(gdp_int[i])
+        pop_den = pop_dens[i]
 
-# Delete collums for the pandas dataframe
-# IN BOTH I ONLY GET 3 COLUMS?????
-# data_countries.drop(data_countries.columns.difference(['Country', 'Region', 'Pop. Density (per sq. mi.)',
-# 'Infant mortality (per 1000 births', 'GDP ($ per capita) dollars)']), 1, inplace=True)
-# print(data_countries)
-# df = data_countries.filter(['Country', 'Region', 'Pop. Density (per sq. mi.)', 'Infant mortality (per 1000 births', 'GDP ($ per capita) dollars)'])
-# 'Infant mortality (per 1000 births', 'GDP ($ per capita) dollars)']]
+        # Make a python dictionary
+        dict_data[country] = {
+            "Region": region,
+            "Pop. Density": pop_den,
+            "Infant mortality (per 1000 births)": infant,
+            "GDP ($ per capita) dollars": gdp_dict
+            }
+
+    # Write the dict_data ot a json file
+    write_to_json("data_file.json", dict_data)
+
+def load_csv(input):
+    """Loads the data from csv file into a pandas dataframe
+       Returns pandas dataframe"""
+
+    data_countries = pd.read_csv(input)
+    return data_countries
+
+def write_to_json(outfile, data):
+    """ Writes data to a json file"""
+
+    with open(outfile, "w") as write_file:
+        json.dump(data, write_file)
+
+def calculate_tendency(column):
+    int_mean = column.mean()
+    int_median = column.median()
+    deviation = column.std()
+    print("Mean: ", int_mean)
+    print("Median: ", int_median)
+    print("Mode: ", column.mode()[0])
+    print("------------------------------")
+
+def calculate_five_number(column):
+    minimum = column.min()
+    maximum = column.max()
+    median = column.median()
+    print("Minimum: ", minimum)
+    print("First Quartile: ", column.quantile(0.25))
+    print("Median: ", median)
+    print("Third Quartile: ", column.quantile(0.75))
+    print("Maximim: ", maximum)
+    print("------------------------------")
+
+if __name__ == "__main__":
+    main()
